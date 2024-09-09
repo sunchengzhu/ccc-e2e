51cea92eb84f1a5cd2b0782c126aaec95e93e9ba
sunchengzhu <sunchengzhu@cryptape.com>
2024-09-09 20:22:26 +0800

    debug

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -42,6 +42,10 @@ const test = baseTest.extend({
         `--disable-extensions-except=${extensionPath}`,
         `--load-extension=${extensionPath}`,
       ],
+      // 添加录屏设置
+      recordVideo: {
+        dir: 'videos/' // 视频将被保存在这个目录
+      }
     });
 
     await use(context);
